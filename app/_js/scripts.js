44fc65050229a91bc50b34abf7668313eae84f88
pavelp21 <pavelvp2182@yandex.ru>
2019-12-11 22:10:52 +0500

    Page is stylezed without experts

--- app/_js/scripts.js
+++ app/_js/scripts.js
@@ -1,8 +1,16 @@
 //@prepros-prepend jquery-2.1.1.min.js
 
-$(document).ready(function() {
+$(document).ready(function () {
+    /* Якорь */
+    $("a[href='#prices']").click(function (h) {
+        h.preventDefault();
+        var f = $(this).attr("href"),
+            g = $(f).offset().top;
+        $("body,html").animate({
+            scrollTop: g
+        }, 1500)
+    });
 
 
-
-/*Конец документа*/
+    /*Конец документа*/
 });
