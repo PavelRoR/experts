0ec745be25068809dcdb8cc5c479232a059ccbc1
pavelp21 <pavelvp2182@yandex.ru>
2020-01-09 12:20:07 +0500

    All Experts are added

--- app/_js/scripts.js
+++ app/_js/scripts.js
@@ -1,5 +1,13 @@
 //@prepros-prepend jquery-2.1.1.min.js
+var isIE = false || !!document.documentMode;
 
+if (isIE) {
+    var head = document.getElementsByTagName("head")[0];
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "css/ie.min.css";
+    head.appendChild(link);
+}
 $(document).ready(function () {
     /* Якорь */
     $("a[href='#prices']").click(function (h) {
